5cc7972becfc7af797e9b9314e3ea7a83c8f84da
asharxh <asharxheikh47@gmail.com>
2026-01-10 12:38:24 +0530

    Algorithm Logic Building

---
+++ src/ashar/CheckPrime.java
@@ -0,0 +1,17 @@
+package ashar;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+        int n = 29;
+        boolean isPrime = n > 1;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        System.out.println(n + (isPrime ? " is prime" : " is not prime"));
+    }
+}
