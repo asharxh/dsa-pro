30825796634c3e1c8117119ce97e4be6db82b03e
asharxh <asharxheikh47@gmail.com>
2026-01-18 16:13:01 +0530

    Count Digit

---
+++ Loops/Counting_and_Condition/SumOfDigitNum.java
@@ -0,0 +1,17 @@
+package Counting_and_Condition;
+
+import java.util.Scanner;
+
+public class SumOfDigitNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Digit");
+        int d = sc.nextInt();
+        int sum = 0;
+        while (d!=0){
+            sum +=d%10;
+            d/=10;
+        }
+        System.out.println(sum);
+    }
+}
