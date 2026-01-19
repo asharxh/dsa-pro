a6a412647efc5b722b67a436dda7eba3df55c85d
asharxh <asharxheikh47@gmail.com>
2026-01-19 18:25:01 +0530

    Check if Number is Prime

---
+++ Math and Numbers/Number_Properties_and_Classification/CheckifNumisPrime.java
@@ -0,0 +1,28 @@
+package Number_Properties_and_Classification;
+
+import java.util.Scanner;
+
+public class CheckifNumisPrime {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Num: ");
+        int n = sc.nextInt();
+        if(isPrime(n)){
+            System.out.println("Prime Num");
+        }
+        else {
+            System.out.println("Not Prime");
+        }
+    }
+    public static boolean isPrime(int n) {
+        if (n<=1){
+            return false;
+        }
+        for (int i = 2; i*i<=n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
