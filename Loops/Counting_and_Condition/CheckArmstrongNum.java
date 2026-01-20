9d5094283f82975255e1dfa2b24b0f8250765a9a
asharxh <asharxheikh47@gmail.com>
2026-01-20 11:05:16 +0530

    Check Armstrong Num and Range

--- Loops/Counting_and_Condition/CheckArmstrongNum.java
+++ Loops/Counting_and_Condition/CheckArmstrongNum.java
@@ -2,21 +2,22 @@ package Counting_and_Condition;
 
 import java.util.Scanner;
 
-class CheckArmstrongNum {
+class CheckArmstrong {
     public static void main(String[] args) {
         armstrongrange();
     }
-        static void armstrongnum() {
+    static void armstrongnum() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Num");
         int n = sc.nextInt();
+        int og = n;
         int sum = 0;
         while (n!=0){
             int digit = n%10;
             sum+=digit*digit*digit;
             n/=10;
         }
-        System.out.println(sum==n? "Armstrong" : "Not Armstrong");
+        System.out.println(sum==og? "Armstrong" : "Not Armstrong");
     }
 
     static void armstrongrange() {
