090318029ec3f014064ddb7930ff053c9fad0547
asharxh <asharxheikh47@gmail.com>
2026-01-20 14:21:38 +0530

    GCD of 2 Numbers

---
+++ Loops/Mathematical_Loops/FindGCDof2Num.java
@@ -0,0 +1,17 @@
+package Mathematical_Loops;
+
+public class FindGCDof2Num {
+    public static void main(String[] args) {
+        int n1 = 24;
+        int n2 = 18;
+
+        while (n2!=0){
+            int temp = n2;
+            n2 = n1%n2;
+            n1 = temp;
+        }
+        System.out.println("GCD = " +n1);
+    }
+}
+
+//Euclidean Algorithm(logic used) - GCD(a,b) = GCD(b, a%b), repeat until b becomes 0. When b ==0 a contain the GCD
