10920321eb42e59b7fc6a4be9f524b4063e36e1e
asharxh <asharxheikh47@gmail.com>
2026-01-20 15:29:01 +0530

    LCM of Two Numbers

---
+++ Loops/Mathematical_Loops/FindLCMof2Num.java
@@ -0,0 +1,22 @@
+package Mathematical_Loops;
+
+import java.util.Scanner;
+
+public class FindLCMof2Num {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Num: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int x = a, y =b;
+
+        while (b!=0){
+            int reminder = b;
+            b = a%b;
+            a = reminder;
+        }
+        int gcd = a;
+        int lcm = (x*y)/gcd;
+        System.out.println("LCM = " + lcm);
+    }
+}
