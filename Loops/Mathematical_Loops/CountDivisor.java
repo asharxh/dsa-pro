0f612f1f083440b8103d69f25ed79d60910a951e
asharxh <asharxheikh47@gmail.com>
2026-01-21 09:44:13 +0530

    Count Print Sum of - Divisor

--- Loops/Mathematical_Loops/CountDivisor.java
+++ Loops/Mathematical_Loops/CountDivisor.java
@@ -8,12 +8,15 @@ public class CountDivisor {
         System.out.println("Enter a Num");
         int n = sc.nextInt();
         int count = 0;
+        int sum = 0;
         for(int i=1; i<=n; i++){
             if(n%i==0){
                 count++;
                 System.out.println(i); //first i printed all divisor in row then count
+                sum += i;
             }
         }
+        System.out.println("Sum = "+ sum);
         System.out.println("count = " + count);
     }
 }
