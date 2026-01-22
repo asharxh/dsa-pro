02264896e41993bc4c2bccd06ff83dc60e8e7c69
asharxh <asharxheikh47@gmail.com>
2026-01-22 16:25:03 +0530

    Print Right Angled Triangle

---
+++ Patterns/Star_Patterns/RightAlignedTriangle.java
@@ -0,0 +1,16 @@
+package Star_Patterns;
+
+public class RightAlignedTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i<=n; i++){
+            for (int space =1; space<=n-1; space++){
+                System.out.println(" ");
+            }
+            for (int star =1; star<=i; star++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
