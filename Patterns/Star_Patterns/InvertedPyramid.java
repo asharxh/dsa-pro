b827dbfe968d8849d2df28be56b47aea0df63454
asharxh <asharxheikh47@gmail.com>
2026-01-26 16:00:00 +0530

    Inverted Pyramid

--- Patterns/Star_Patterns/InvertedPyramid.java
+++ Patterns/Star_Patterns/InvertedPyramid.java
@@ -1,6 +1,6 @@
 package Star_Patterns;
 
-public class InvvertedPyramid {
+public class InvertedPyramid {
     public static void main(String[] args) {
         int n = 5;
 
@@ -15,3 +15,13 @@ public class InvvertedPyramid {
         }
     }
 }
+
+
+//Note for me:
+/*
+Look carefully at outer look statement. When we are Inverting i start from n, so if i>=1 and i--
+Inner Look spacing and star printing logic are same in regular and inverted.
+During Execution first 1 time outer look will run then by using outer loop condition, inner loop will run
+multiple time till condition match.
+
+ */
