6a35806e57735c22cc94edbd2538572b52d22b6f
asharxh <asharxheikh47@gmail.com>
2026-01-27 11:07:45 +0530

    Plus Pattern

---
+++ Patterns/Star_Patterns/PrintPlusPattern.java
@@ -0,0 +1,21 @@
+package Star_Patterns;
+
+public class PrintPlusPattern {
+    public static void main(String[] args) {
+
+        int n = 5;
+        int mid = n/2;
+        for(int i=0; i<n; i++){
+            for (int j = 0; j<n; j++){
+                if(i==mid || j ==mid){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
