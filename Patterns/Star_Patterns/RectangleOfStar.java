f8c170cf9a2f25cba9390c9bea0b5a005ac03ba0
asharxh <asharxheikh47@gmail.com>
2026-01-27 11:34:35 +0530

    Rectangle of Stars

---
+++ Patterns/Star_Patterns/RectangleOfStar.java
@@ -0,0 +1,15 @@
+package Star_Patterns;
+
+public class RectangleOfStar {
+    public static void main(String[] args) {
+        int row = 4;
+        int col = 16;
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
