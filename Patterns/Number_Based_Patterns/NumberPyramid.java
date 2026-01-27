eede259b33bd79fb7a033cca890917c2f9166cc2
asharxh <asharxheikh47@gmail.com>
2026-01-27 15:23:54 +0530

    Number Pyramid

---
+++ Patterns/Number_Based_Patterns/NumberPyramid.java
@@ -0,0 +1,20 @@
+package Number_Based_Patterns;
+
+public class NumberPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int space = 1; space <= n - i; space++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            for (int j = i-1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
