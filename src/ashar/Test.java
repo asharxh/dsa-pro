bce1a9f29bf9bcedd885c627250b027d08b863ea
asharxh <asharxheikh47@gmail.com>
2026-01-27 17:58:28 +0530

    Test

--- src/ashar/Test.java
+++ src/ashar/Test.java
@@ -1,5 +1,20 @@
 package ashar;
 
 public class Test {
+    public void mySqrt(int x) {
+            if (x < 2) return;
 
-}
+            int left = 1, right = x / 2;
+            int ans = 0;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (mid <= x / mid) {
+                    ans = mid;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+    }
