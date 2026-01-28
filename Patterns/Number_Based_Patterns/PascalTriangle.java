04c310ae9c9c91fe974def178925632379081773
asharxh <asharxheikh47@gmail.com>
2026-01-28 12:23:26 +0530

    Pascal Triangle (L:118-Easy)

--- Patterns/Number_Based_Patterns/PascalTriangle.java
+++ Patterns/Number_Based_Patterns/PascalTriangle.java
@@ -3,7 +3,6 @@ package Number_Based_Patterns;
 public class PascalTriangle {
     public static void main(String[] args) {
         int n = 5;
-
         for(int i=0; i<n; i++){
             for(int s=1; s<=n-1; s++){
                 System.out.print(" ");
