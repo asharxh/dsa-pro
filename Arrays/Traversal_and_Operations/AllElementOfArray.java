34601d3bb5954c8963c307c7745d936bfad2800e
asharxh <asharxheikh47@gmail.com>
2026-01-28 12:15:19 +0530

    Print All Element of Array

---
+++ Arrays/Traversal_and_Operations/AllElementOfArray.java
@@ -0,0 +1,11 @@
+package Traversal_and_Operations;
+
+public class AllElementOfArray {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 4, 6, 8, 1, 9};
+        for(int i = 0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
