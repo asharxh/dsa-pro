ce4bb341721fda2415d00566e19e2fa4343c2abf
asharxh <asharxheikh47@gmail.com>
2026-01-28 15:05:01 +0530

    Replace All Negative With 0

--- Arrays/Traversal_and_Operations/ReplaceAllNegW0.java
+++ Arrays/Traversal_and_Operations/ReplaceAllNegW0.java
@@ -2,7 +2,6 @@ package Traversal_and_Operations;
 
 public class ReplaceAllNegW0 {
     public static void main(String[] args) {
-
         int[] arr = {-2,4,-3,5,6,-9,-1};
         int neg = 0;
         for(int i = 0; i<arr.length; i++){
