f111799e0b2cc7b93c154f7c9dd5bce9a0cfa166
asharxh <asharxheikh47@gmail.com>
2026-01-30 17:29:14 +0530

    Element Appear 1

---
+++ Arrays/Rotation_and_Rearrangement/ElementAppearOnce.java
@@ -0,0 +1,27 @@
+package Rotation_and_Rearrangement;
+
+public class ElementAppearOnce {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 2, 3, 2,3,1,1,2,3};
+        int single = -1;
+        for(int i=0; i<nums.length; i++){
+            int count = 0;
+
+            for(int j = 0; j<nums.length; j++){
+                if (nums[i] == nums[j]){
+                    count++;
+                }
+            }
+            if(count ==1){
+                single = nums[i];
+                break;
+            }
+        }
+        System.out.println(single);
+    }
+}
+
+
+/*
+This is brute force approach O(n2). Its not optimal
+ */
