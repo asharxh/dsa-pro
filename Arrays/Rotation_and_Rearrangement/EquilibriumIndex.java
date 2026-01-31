8bc669e93f4f4b0a905651c4f3b2904b7122c879
asharxh <asharxheikh47@gmail.com>
2026-01-31 23:46:29 +0530

    Equilibrium Index

---
+++ Arrays/Rotation_and_Rearrangement/EquilibriumIndex.java
@@ -0,0 +1,24 @@
+package Rotation_and_Rearrangement;
+
+public class EquilibriumIndex {
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,4,3,2,5,3,4,6};
+        int n = arr.length;
+        int equilibrium = -1;
+        for(int i=0; i<n; i++){
+            int leftsum = 0, rightsum = 0;
+            for(int j = 0; j<i; j++){
+                leftsum += arr[j];
+            }
+            for(int j =i+1; j <n; j++){
+                rightsum += arr[j];
+            }
+
+            if(leftsum==rightsum){
+                equilibrium=i;
+                break;
+            }
+        }
+        System.out.println("Equilibrium Index = " + equilibrium);
+    }
+}
