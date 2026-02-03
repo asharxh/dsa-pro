a14d199e6f924eca7d26196b3fb53bf75b2a5252
asharxh <asharxheikh47@gmail.com>
2026-02-03 12:25:11 +0530

    Sum of last K

---
+++ Prefix Sum/Prefix_Sum_Basics/SumOfLastK.java
@@ -0,0 +1,17 @@
+package Prefix_Sum_Basics;
+
+public class SumOfLastK {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6};
+
+        int[] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+        int k = 3;
+        int sum = 0;
+        for(int i =arr.length-k; i<arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
