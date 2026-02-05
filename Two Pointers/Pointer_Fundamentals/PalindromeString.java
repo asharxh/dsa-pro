96fef1bf3a044f57ffa616cc09b74751828b261a
asharxh <asharxheikh47@gmail.com>
2026-02-05 09:15:21 +0530

    Palindrome String

---
+++ Two Pointers/Pointer_Fundamentals/PalindromeString.java
@@ -0,0 +1,32 @@
+package Pointer_Fundamentals;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+
+        String str = "A man, a plan";
+        int l = 0;
+        int r = str.length() - 1;
+
+        boolean isPalindrome = true;
+        while (l<r){
+            char cl = str.charAt(l);
+            char cr = str.charAt(r);
+
+            if (!Character.isLetterOrDigit(cl)){
+                l++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(cr)){
+                r--;
+                continue;
+            }
+            if(Character.toLowerCase(cl) != Character.toLowerCase(cr)){
+                isPalindrome =false;
+                break;
+            }
+            l++;
+            r--;
+        }
+        System.out.println(isPalindrome);
+    }
+}
