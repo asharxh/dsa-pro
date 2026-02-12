488a1ed8504eecbe546bebf7f2988921ef0f7ddd
asharxh <asharxheikh47@gmail.com>
2026-02-12 14:55:13 +0530

    Count Frequency of Element

--- Hash Maps and Sets/Frequency_Counting_and_Lookup/CountFreqOfEleInArr.java
+++ Hash Maps and Sets/Frequency_Counting_and_Lookup/CountFreqOfEleInArr.java
@@ -9,8 +9,28 @@ public class CountFreqOfEleInArr {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i=0; i<arr.length; i++){
-            map.put(i, map.getOrDefault(i, 0)+1);
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
         }
         System.out.println(map);
     }
 }
+
+
+/*
+Note for me:
+A HashMap<K,V> stores key → value pairs
+Here:
+Key   = Integer
+Value = Integer
+In frequency counting:
+Key = array element
+Value = number of times it appears
+Example
+1 - 2
+3 - 1
+8 - 2
+6 - 1
+Loop will iterate for map.put(i, map.getOrDefault(i, 0)+1);
+getOrDefault()  - If key i exists → return its value
+If key i does NOT exist → return 0
+ */
