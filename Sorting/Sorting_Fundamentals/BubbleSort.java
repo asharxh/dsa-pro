fbacc17f910c6a818b704da81564aff7392a6b28
asharxh <asharxheikh47@gmail.com>
2026-03-01 15:39:48 +0530

    Bubble Sort

--- Sorting/Sorting_Fundamentals/BubbleSort.java
+++ Sorting/Sorting_Fundamentals/BubbleSort.java
@@ -15,7 +15,7 @@ public class BubbleSort {
             }
         }
         for(int i = 0; i<n; i++) {
-            System.out.println(arr[i]);
+            System.out.println(arr[i] + " ");
         }
     }
 }
