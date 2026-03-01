c4bd435dce535c1586ede0d280723f40148f3954
asharxh <asharxheikh47@gmail.com>
2026-03-01 16:13:10 +0530

    Bubble Sort

--- Sorting/Sorting_Fundamentals/BubbleSort.java
+++ Sorting/Sorting_Fundamentals/BubbleSort.java
@@ -1,10 +1,19 @@
 package Sorting_Fundamentals;
 
+import java.util.Random;
+
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {12,4,5,2,7,9,3};
+        Random r = new Random();
+        int[] arr = new int[100];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = r.nextInt(100000);
+        }
+        System.out.println("Before: ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
         int n = arr.length;
-
         for(int i=0; i<n-1; i++){
             for(int j =0; j<n-i-1; j++){
                 if(arr[j] > arr[j+1]){
@@ -14,8 +23,10 @@ public class BubbleSort {
                 }
             }
         }
+        System.out.println();
+        System.out.println("After: ");
         for(int i = 0; i<n; i++) {
-            System.out.println(arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
     }
 }
